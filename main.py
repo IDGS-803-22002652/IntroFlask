44a0560a267bc23ad3b2b12fa47341a5dc79191b
IDGS-803-22002652 <81013@alumnos.utleon.edu.mx>
2025-01-31 00:10:20 -0600

    formularios

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return"Hola mundo!!! Nuevo"
+    grupo="IDGS803"
+    lista=["Omar","Luis","Jorge","Erick","Javier"]
+    return render_template('index.html',grupo=grupo,lista=lista)
+
+#@app.route('/OperasBas')
+#def OperasBas():
+#return render_template('OperasBas.html')
+
+@app.route('/OperasBas',methods=['POST','GET']) 
+def OperasBas():
+    if request.method=='POST':
+        num1=request.form.get("num1")
+        num2=request.form.get("num2")
+        resultado=int(num1)+int(num2)
+        return render_template('OperasBas.html',resultado=resultado)
+    else:
+        return render_template('OperasBas.html')
+ 
+@app.route('/ejemplo1')
+def ejemplo1():
+    return render_template('ejemplo1.html')
+
+@app.route('/ejemplo2')
+def ejemplo2():
+    return render_template('ejemplo2.html')
+
 
 @app.route('/hola')
 def hola():
@@ -13,7 +38,7 @@ def hola():
 @app.route('/user/<string:user>')
 def user(user):
     return"Hola "+user  
-    return f"Hola {user}"
+    #return f"Hola {user}"
 
 @app.route('/numeri/<int:n>')
 def numeri(n):
@@ -30,8 +55,16 @@ def suma(n1,n2):
 @app.route("/default")   
 @app.route("/default/string:nom")
 def function(nom="Omar"):
-    return "Hola {}".format(nom)   
+    return "Hola {}".format(nom) 
 
+@app.route("/form1")  
+def form1():
+    return'''
+            <form>
+            <label>Nombre:</label>
+            <input type="text" name="nombre" placeholder="Nombre></input>
+            </form>
+        '''
 
 if __name__ == '__main__':
     app.run(debug=True,port=3000)
